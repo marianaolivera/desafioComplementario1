0d876901476e7cb0311ae5c5a563fa783359ad4d
Mariana <marianaolivera83@gmail.com>
2022-06-20 23:43:43 -0300

    agregue condicion if else

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /*este algoritmo calcula un carrito de compras,multiplica el numero de productos por el valor unitario y devuelve el total*/
 let numero = 0
 numero = prompt("Ingresar Cantidad")
+ if (numero >10){
+    alert("sin stock")
+ }else 
 Total=numero * 3500
 alert ("$"+ Total)
