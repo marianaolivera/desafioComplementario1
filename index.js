3d9bcba283424ef31c7f4a9a1159021fd3cf11e1
Mariana <marianaolivera83@gmail.com>
2022-06-20 23:34:34 -0300

    codigo js

---
+++ index.js
@@ -0,0 +1,5 @@
+/*este algoritmo calcula un carrito de compras,multiplica el numero de productos por el valor unitario y devuelve el total*/
+let numero = 0
+numero = prompt("Ingresar Cantidad")
+Total=numero * 3500
+alert ("$"+ Total)
